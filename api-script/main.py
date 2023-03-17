ab768ea2697d693c33acc8040feb149c98653ae2
mohamedmohsen <113861038+mohummedmohsen@users.noreply.github.com>
2023-03-17 21:24:15 +0200

    Update main.py

--- api-script/main.py
+++ api-script/main.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get("https://gitlab.com/api/v4/users/mohamedmohsenali38/projects")
+response = requests.get("<your URL>")
 my_projects = response.json()
 
 for project in my_projects:

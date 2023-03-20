4c72d0f2d53927b73138f00fe28ebcc406f3e78d
muhammedmohsen <mohamedmohsenali38@gmail.com>
2023-03-20 21:49:34 +0200

    scheduled volume backups using python

---
+++ aws/volume-backups.py
@@ -0,0 +1,21 @@
+import boto3 
+import schedule
+
+ec2_client = boto3.client('ec2')
+
+def create_volume_snapshots():
+    volumes = ec2_client.describe_volumes()
+    print(volumes['Volumes'])
+
+    for volume in volumes['Volumes']:
+        new_snapshot = ec2_client.create_snapshot(
+            VolumeId=volume['VolumeId']
+        )
+
+        print(new_snapshot)
+
+schedule.every().day.do(create_volume_snapshots)
+
+while True:
+    schedule.run_pending()
+
